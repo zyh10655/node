fdbb55051b6b0d524175d8e00bc2fcf3f72d9e6f
NokiaZ <49866374+zyh10655@users.noreply.github.com>
2020-10-13 15:58:02 +1300

    Update index.js

--- node-js-getting-started-main/index.js
+++ node-js-getting-started-main/index.js
@@ -8,3 +8,4 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//Care with the packages used in package.json
