bfd6a10fd9ec7bee3808d9f034285343a424c2d5
NokiaZ <49866374+zyh10655@users.noreply.github.com>
2020-10-13 16:05:20 +1300

    Update test.js

--- node-js-getting-started-main/test.js
+++ node-js-getting-started-main/test.js
@@ -26,3 +26,24 @@ test('responds to requests', (t) => {
     })();
   });
 });
+// New note post example:  
+// app.post('/notes', (request, response) => {
+//     const body = request.body
+   
+//     if(!body.content) { //no content
+//         return response.status(400).json({
+//             error: 'content missing'
+//         })
+//     }
+
+//     const note = {
+//         content: body.content,
+//         important: body.important || false,
+//         date: new Date(),
+//         id: generateId(),
+//     }
+
+//     notes = notes.concat(note)
+
+//     response.json(note)
+//   })
